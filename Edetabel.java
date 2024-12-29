67a67f66c06f1c47304e79f5d3c1d0d79aa277c6
liisa-j <liisa.jullinen>
2024-12-29 19:38:32 +0200

    Elleni muudatused lisaks

--- Edetabel.java
+++ Edetabel.java
@@ -1,11 +1,17 @@
 package oop;
 
+import java.io.*;
+import java.time.*;
+import java.time.format.DateTimeParseException;
+
+
 public class Edetabel {
     private int minuVõidud;
     private int vastaseVõidud;
     private int mängeMängitud;
     private int minuKäigud;
     private int vastaseKäigud;
+    private LocalDateTime uusAeg;
 
     public Edetabel() {
         this.minuVõidud = 0;
@@ -13,6 +19,7 @@ public class Edetabel {
         this.mängeMängitud = 0;
         this.minuKäigud = 0;
         this.vastaseKäigud = 0;
+        this.uusAeg = LocalDateTime.now();
     }
     public void addMinuVõidud() {
         this.minuVõidud++;
@@ -26,33 +33,51 @@ public class Edetabel {
     public void addMinuKäigud() {
         this.minuKäigud++;
     }
-    public void addVastaseKäigud() {
-        this.vastaseKäigud++;
-    }
+    public void addVastaseKäigud() {this.vastaseKäigud++;}
+
     public void statistika() {
-        System.out.println("Kõikide mängude arv " +mängeMängitud + "." );
         System.out.println("Minu võidud " + minuVõidud + " ning minu käigud " + minuKäigud + ".");
         System.out.println("Vastase võidud " + vastaseVõidud + " ning tema käigud " + vastaseKäigud + ".");
-
+        System.out.println("Kõikide mängude arv " +mängeMängitud + "." );
+        System.out.println("Minu käigud " + minuKäigud);
+        System.out.println("Vastase käigud " + vastaseKäigud);
+        System.out.println("Edetabeli statistika alates " + uusAeg);
     }
 
-    public int getMinuVõidud() {
-        return minuVõidud;
-    }
+    public void salvestaEdetabel (String failinimi) throws SisendiErind {
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(failinimi))) {
+            bw.write("Minu võidud " + minuVõidud + "\n");
+            bw.write("Vastase võidud " + vastaseVõidud +  "\n");
+            bw.write("Kõikide mängude arv " +mängeMängitud + "\n");
+            bw.write("Minu käigud " +minuKäigud+ "\n");
+            bw.write("Vastase käigud " +vastaseKäigud + "\n");
+            bw.write("Edetabeli statistika alates " + uusAeg + "");
+            System.out.println(failinimi + " failis on Edetabeli statistika edukalt salvestatud.");
+        } catch (IOException e) {
+            throw new SisendiErind("Tekkis viga faili salvestamisel" + e.getMessage());
+        }
+    }public void loeEdetabelist (String failinimi) throws SisendiErind {
+        try(BufferedReader br = new BufferedReader(new FileReader(failinimi))) {
+            minuVõidud = Integer.parseInt(br.readLine().replace("Minu võidud ", "").trim());
+            vastaseVõidud = Integer.parseInt(br.readLine().replace("Vastase võidud ", "").trim());
+            mängeMängitud = Integer.parseInt(br.readLine().replace("Kõikide mängude arv ", "").trim());
+            minuKäigud = Integer.parseInt(br.readLine().replace("Minu käigud ", "").trim());
+            vastaseKäigud = Integer.parseInt(br.readLine().replace("Vastase käigud ", "").trim());
+            uusAeg = LocalDateTime.parse(br.readLine().replace("Edetabeli statistika alates ", "").trim());
+            System.out.println(failinimi + " failist loetud Edetabeli statistika");
+        } catch (FileNotFoundException e)  {
+            throw new SisendiErind("Puudub sellise nimega fail (" + failinimi + ")");
+        } catch (IOException e) {
+            throw new SisendiErind("Viga faili lugemisel " + e.getMessage());
+        }
 
-    public int getVastaseVõidud() {
-        return vastaseVõidud;
-    }
-
-    public int getMängeMängitud() {
-        return mängeMängitud;
-    }
-
-    public int getMinuKäigud() {
-        return minuKäigud;
-    }
-
-    public int getVastaseKäigud() {
-        return vastaseKäigud;
+    }public void tagasiNulli() {
+        this.minuVõidud = 0;
+        this.vastaseVõidud = 0;
+        this.mängeMängitud = 0;
+        this.minuKäigud = 0;
+        this.vastaseKäigud = 0;
+        this.uusAeg = LocalDateTime.now();
+        System.out.println("Edetabeli statistika on kustutatud");
     }
 }
--- PäkapikkudePommitamine.java
+++ PäkapikkudePommitamine.java
@@ -1,7 +1,6 @@
 package oop;
 
-
-import java.io.*;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class PäkapikkudePommitamine {
@@ -23,121 +22,133 @@ public class PäkapikkudePommitamine {
         tutvustusTekst();
         char mets = 'M';
         Edetabel info = new Edetabel();
-
+        String failinimi = "päkapikud.txt";
 
         Scanner in = new Scanner(System.in);
-        while (true) {
-            System.out.println();
-            System.out.println("Sisesta mänguvälja pikkus: ");
-            int mänguväljapikkus = in.nextInt();
-            while (mänguväljapikkus < 3 || mänguväljapikkus > 10) {
-                System.out.println("Proovi uuesti, mänguvälja vahemik on 3-10 ruutu");
-                mänguväljapikkus = in.nextInt();
-            }
 
-
-            // loome mänguväljad ühele mängijale
-            mänguVäli minu = new mänguVäli();
-            minu.looMänguväli(mänguväljapikkus, mets);
-            // minu.prindiMänguväli();
-            System.out.println();
-
-
-            System.out.println("Kas soovid valida ise päkapikkude asukohti või panna paika juhuslikult: (1/2)");
-            int minuvalik = 0;
-            while (minuvalik !=1 && minuvalik !=2) {
-                if (in.hasNextInt()) {
-                    minuvalik = in.nextInt();
-                    if (minuvalik == 1) {
-                        minu.arvutaPäkapikud();
-                        minu.valinPäkapikud();
-                        System.out.println("Selles mängus osaleb päkapikke: " + minu.getPäkapikud());
-                    }else if (minuvalik == 2) {
-                        minu.arvutaPäkapikud();
-                        minu.lisapikud();
-                        System.out.println("Selles mängus osaleb kummalgi mängijal päkapikke: " + minu.getPäkapikud());
-                    }else {
-                        System.out.println("Proovi uuesti: 1 - valin ise päkapikud, 2 - päkapikkude juhuslikult paigutus");
-                    }
-                } else { System.out.println("Proovi sisestada number: 1 - valin ise päkapikud, 2 - päkapikkude juhuslikult paigutus");
-                    in.next();
-                }
-            }
-
-
-            // loome mänguvälja teisele (arvutile):
-            VastaseMänguVäli sinu = new VastaseMänguVäli();
-            sinu.looMänguväli(mänguväljapikkus, mets);
-            //sinu.prindiMänguväli();
-            System.out.println();
-            sinu.arvutaPäkapikud();
-            sinu.lisapikud();
-
-            // Siin teeme esialgse salvestuse mänguseisust (et vältida viga, mis tekib kui mängija üritab salvestamata mängu taastada)
-            minu.salvestus();
-            sinu.salvestus();
-
-            do {
-                // Siin pakutakse iga käigu alguses võimalust mänguseisu salvestada või salvestust taastada
-                System.out.println("Kui soovid mänguseisu salvestada, vajuta klahvile s, kui soovid taasatada mängu varem salvestatud seisu, vajuta klahvile t, " +
-                        "jätkamiseks vajuta ükskõik, mis tähte!");
-                String salvestus = in.next();
-
-
-                // Mänguseisu salvestamine
-                if (salvestus.equals("s")) {
-                    System.out.println("Mänguseis on salvestatud!");
-                    minu.salvestus();
-                    sinu.salvestus();
-                }
-
-                // Salvestatud mänguseisu taastamine
-                else if(salvestus.equals("t")){
-                    System.out.println("Taastame mängu salvestuspunkti...");
-                    minu.taastamine();
-                    sinu.taastamine();
-                }
-
-
-                else {
-
-                    System.out.println("Vastasel on siin peidus päkapikke: " + sinu.getPäkapikud());
-                    sinu.prindiMänguväli();
-                    info.addMinuKäigud();
-                    sinu.pommita();
-
-                    System.out.println();
-                    System.out.println("Minul on alles päkapikke: " + minu.getPäkapikud());
-                    info.addVastaseKäigud();
-                    minu.pommita();
-                    minu.prindiMänguväli();
-                }
-            } while ((minu.getPäkapikud() > 0) && (sinu.getPäkapikud() > 0));
-
-            System.out.println("Mäng sai läbi!");
-            if (minu.getPäkapikud() == 0) {
-                System.out.println("Kahjuks kaotasid mängu! Parem õnn järgmisel korral!");
-                info.addVastaseVõidud();
-                info.addMängeMängitud(); //LISATUD
-            } else {
-                System.out.println("Juhuuu, võitsid!");
-                info.addMinuVõidud();
-                info.addMängeMängitud(); //LISATUD
-            }
-            info.statistika();
-
-            System.out.println("Kas teeme uue mängu? (jah/ei)");
-            String uusmäng = in.next();
-            if (uusmäng.equalsIgnoreCase("ei")) {
-                System.out.println("Järgmise korrani!");
-                break;
-            }
-
-
+        try{
+            info.loeEdetabelist(failinimi);
+        }catch (SisendiErind e) {
+            System.err.println(e.getMessage());
         }
 
+        while (true) {
+            System.out.println("Mida sooviksid teha?");
+            System.out.println("1 - vaadata edetabelit, 2 - alustada uut mängu, 3 - kustutada edetabeli, 4 - lahkuda mängust");
+            int valitud = in.nextInt();
 
+            if (valitud == 2) {
+                System.out.println("Sisesta mänguvälja pikkus: ");
+                int mänguväljapikkus = in.nextInt();
+                while (mänguväljapikkus < 3 || mänguväljapikkus > 10) {
+                    System.out.println("Proovi uuesti, mänguvälja vahemik on 3-10 ruutu");
+                    mänguväljapikkus = in.nextInt();
+                }
+                // loome mänguväljad ühele mängijale
+                mänguVäli minu = new mänguVäli();
+                minu.looMänguväli(mänguväljapikkus, mets);
+                // minu.prindiMänguväli();
+                System.out.println();
+
+
+                System.out.println("Kas soovid valida ise päkapikkude asukohti või panna paika juhuslikult: (1/2)");
+                int minuvalik = 0;
+                while (minuvalik != 1 && minuvalik != 2) {
+                    if (in.hasNextInt()) {
+                        minuvalik = in.nextInt();
+                        if (minuvalik == 1) {
+                            minu.arvutaPäkapikud();
+                            minu.valinPäkapikud();
+                            System.out.println("Selles mängus osaleb päkapikke: " + minu.getPäkapikud());
+                        } else if (minuvalik == 2) {
+                            minu.arvutaPäkapikud();
+                            minu.lisapikud();
+                            System.out.println("Selles mängus osaleb kummalgi mängijal päkapikke: " + minu.getPäkapikud());
+                        } else {
+                            System.out.println("Proovi uuesti: 1 - valin ise päkapikud, 2 - päkapikkude juhuslikult paigutus");
+                        }
+                    } else {
+                        System.out.println("Proovi sisestada number: 1 - valin ise päkapikud, 2 - päkapikkude juhuslikult paigutus");
+                        in.next();
+                    }
+                }
+
+
+                // loome mänguvälja teisele (arvutile):
+                VastaseMänguVäli sinu = new VastaseMänguVäli();
+                sinu.looMänguväli(mänguväljapikkus, mets);
+                //sinu.prindiMänguväli();
+                System.out.println();
+                sinu.arvutaPäkapikud();
+                sinu.lisapikud();
+
+                // Siin teeme esialgse salvestuse mänguseisust (et vältida viga, mis tekib kui mängija üritab salvestamata mängu taastada)
+                minu.salvestus();
+                sinu.salvestus();
+
+                do {
+
+                    // Siin pakutakse iga käigu alguses võimalust mänguseisu salvestada või salvestust taastada
+                    System.out.println("Mida soovid teha?" +  "\n" + "s - salvesta mänguseis" + "\n" + "t - taasata mängu salvestatud seis" + "\n" + "ükskõik, mis täht/number - jätka mängu!");
+                    String salvestus = in.next();
+
+
+                    // Mänguseisu salvestamine
+                    if (salvestus.equals("s")) {
+                        System.out.println("Mänguseis on salvestatud!");
+                        minu.salvestus();
+                        sinu.salvestus();
+                    }
+
+                    // Salvestatud mänguseisu taastamine
+                    else if(salvestus.equals("t")){
+                        System.out.println("Taastame mängu salvestuspunkti...");
+                        minu.taastamine();
+                        sinu.taastamine();
+                    }
+
+                    else {
+
+                        System.out.println("Vastasel on siin peidus päkapikke: " + sinu.getPäkapikud());
+                        sinu.prindiMänguväli();
+                        info.addMinuKäigud();
+                        sinu.pommita();
+
+                        System.out.println();
+                        System.out.println("Minul on alles päkapikke: " + minu.getPäkapikud());
+                        info.addVastaseKäigud();
+                        minu.pommita();
+                        minu.prindiMänguväli();
+                    }
+
+                } while ((minu.getPäkapikud() > 0) && (sinu.getPäkapikud() > 0));
+
+
+                System.out.println("Mäng sai läbi!");
+                if (minu.getPäkapikud() == 0) {
+                    System.out.println("Kahjuks kaotasid mängu! Parem õnn järgmisel korral!");
+                    info.addVastaseVõidud();
+                    info.addMängeMängitud(); //LISATUD
+                } else {
+                    System.out.println("Juhuuu, võitsid!");
+                    info.addMinuVõidud();
+                    info.addMängeMängitud(); //LISATUD
+                }
+            }else if (valitud == 1) {
+                info.statistika();
+            }else if (valitud == 3) {
+                info.tagasiNulli();
+            }else if (valitud == 4) {
+                try {
+                    info.salvestaEdetabel(failinimi);
+                } catch (SisendiErind e) {
+                    System.err.println("Viga faili salvestusel" + e.getMessage());
+                }
+                break;
+            }else {
+                System.out.println("Ebasobiv sisend!");
+            }
+        } in.close();
     }
-
 }
 
